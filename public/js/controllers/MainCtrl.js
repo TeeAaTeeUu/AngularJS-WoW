55c4d018f9c13847df1cadabd0685acd6a806d75
TeeAaTeeU <teeaateeuu@gmail.com>
2014-05-06 18:08:03 +0300

    Search is now more beautiful with bootstrap

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,7 +3,17 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 	$scope.tagline = 'Here be stuff';
 
 	$scope.search = function(itemID) {
-		$scope.result = "here be the good stuff";
+		if (itemID) {
+			$scope.result = "here be the good stuff";
+		} else {
+			$scope.result = "You didn't give me anything. Why?";
+		};
+		
 	};
+
+	$scope.clearSearch = function() {
+		$scope.result = null;
+		$scope.itemID = null;
+	}
 
 });
