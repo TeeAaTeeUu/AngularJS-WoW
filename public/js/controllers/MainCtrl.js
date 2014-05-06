da6a9dccbf5a4cc95d877b8a08b87f4c516c8838
TeeAaTeeU <teeaateeuu@gmail.com>
2014-05-06 14:04:09 +0300

    testing and getting the form work

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,9 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'Here be stuff';	
+	$scope.tagline = 'Here be stuff';
+
+	$scope.search = function(itemID) {
+		$scope.result = "here be the good stuff";
+	};
 
 });
