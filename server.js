2421f592a63fa30ea5d7ce20decc6e560a2f26f1
TeeAaTeeU <teeaateeuu@gmail.com>
2014-05-07 13:01:40 +0300

    Fixing removed old configure from latest express

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ var app     = express();
 
 var port = process.env.PORT || 8080; // set our port
 
-app.configure(function() {
-	app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-	app.use(express.logger('dev')); 					// log every request to the console
-});
+app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
